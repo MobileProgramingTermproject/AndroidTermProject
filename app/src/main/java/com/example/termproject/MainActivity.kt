3744aff6ad599960abba59c231e61ed4bddbca0c
Jeonghan Kim <gtrew9606@naver.com>
2022-11-04 14:36:49 +0900

    kimjeonghan

--- app/src/main/java/com/example/termproject/MainActivity.kt
+++ app/src/main/java/com/example/termproject/MainActivity.kt
@@ -9,5 +9,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         println("aaaaaaaa");
+        println("jeonghan");
+        println("jeonghan");
     }
 }
