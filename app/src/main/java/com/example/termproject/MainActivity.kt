3bf7a3476f7476e443a54d5c53c0f47e0185d348
tjdgns99 <tjdgns1657@naver.com>
2022-11-12 01:49:24 +0900

    gradle

--- app/src/main/java/com/example/termproject/MainActivity.kt
+++ app/src/main/java/com/example/termproject/MainActivity.kt
@@ -1,17 +1,43 @@
 package com.example.termproject
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() { //로그인
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.login_layout) //login_layout 열기
 
-        println("aaaaaaaa");
-        println("jeonghan");
-        println("jeonghan");
+        val ID = findViewById<EditText>(R.id.editTextTextPersonName).text
+        val PassWord = findViewById<EditText>(R.id.editTextTextPassword).text
 
-        println("aaaaaaaaaaaaaa")
+        findViewById<Button>(R.id.button_login).setOnClickListener{ //로그인 버튼 클릭
+            Firebase.auth.signInWithEmailAndPassword(ID.toString(), PassWord.toString()) //파이어베이스 로그인 시도
+                .addOnCompleteListener {
+                    if(it.isSuccessful) {//성공 시 표시
+                        println("######Login Success ${ID} ${PassWord}")
+                        findViewById<TextView>(R.id.CheckLogin).setText("Login Success ${ID} ${PassWord}") //화면에 표시
+                    }else {//실패 시 표시
+                        println("######Login Failed ${it.exception?.message} ${ID} ${PassWord}")
+                        findViewById<TextView>(R.id.CheckLogin).setText("Login Failed ${it.exception?.message} ${ID} ${PassWord}") //화면에 표시
+                    }
+                }
+        }
+        findViewById<TextView>(R.id.membershiptxt).setOnClickListener { //회원가입 클릭 시 membership액티비티로
+            val intent = Intent(this, MembershipActivity::class.java) //intent 생성 this에서 MembershipActivity로 이동
+            startActivity(intent)
+        }
+        
+
+
+
     }
 }
--- app/src/main/java/com/example/termproject/MembershipActivity.kt
+++ app/src/main/java/com/example/termproject/MembershipActivity.kt
@@ -2,10 +2,48 @@ package com.example.termproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MembershipActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_membership)
+        setContentView(R.layout.membership_layout)  //회원가입 레이아웃으로
+
+        val Id = findViewById<EditText>(R.id.ID).text
+        val Password = findViewById<EditText>(R.id.Password).text
+
+        findViewById<Button>(R.id.Check).setOnClickListener {   //비밀번호 확인
+            if (Password.toString()
+                    .equals(findViewById<EditText>(R.id.PasswordCheck).text.toString())
+            ) {
+                println("######Password Checked");
+                findViewById<TextView>(R.id.checktextView).setText("Checked")   //일치하면 표시
+            } else{
+                findViewById<EditText>(R.id.PasswordCheck).setText("")          //불일치 시 비밀번호 확인 EditTextView 비우기
+                findViewById<TextView>(R.id.checktextView).setText("Check Failed")   //일치하면 표시
+           }
+        }
+
+        findViewById<Button>(R.id.Membership_btn).setOnClickListener {
+            if(findViewById<TextView>(R.id.checktextView).text.toString().equals("Checked")) { //비밀번호 확인이 됬으면 Firebase Signup 시도
+                Firebase.auth.createUserWithEmailAndPassword(Id.toString(), Password.toString())
+                    .addOnCompleteListener {
+                        if (it.isSuccessful) {
+                            println("######Sign-up Success")
+                            finish()    //성공했으면 원래 액티비티로 복귀
+                        } else {
+                            println("######Sign-up Login Failed ${it.exception?.message}")
+                            findViewById<TextView>(R.id.checktextView).setText("Signup failed ${it.exception?.message}")
+                        }
+                    }
+            }else{
+                findViewById<TextView>(R.id.checktextView).setText("Checked Please")
+
+            }
+        }
     }
 }
