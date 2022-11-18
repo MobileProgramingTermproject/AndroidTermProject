d03fb5a97530a0db906df4ca18921744f56057ba
Jeonghan Kim <gtrew9606@naver.com>
2022-11-18 15:55:23 +0900

    StorageActivity, activity_storage 파일 새로 만들어서 스토리지 실험해봄. 그걸 응용해서 AddPostActivity에다 수정할 계획

--- app/src/main/java/com/example/termproject/AddPostActivity.kt
+++ app/src/main/java/com/example/termproject/AddPostActivity.kt
@@ -1,28 +1,44 @@
 package com.example.termproject
 
-import android.graphics.BitmapFactory
+import android.content.DialogInterface
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageView
+import android.widget.Toast
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.ActivityResultLauncher
 import androidx.appcompat.app.AppCompatActivity
+import com.example.termproject.databinding.AddpostLayoutBinding
+import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.ktx.storage
 
+
 class AddPostActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.addpost_layout)
+    lateinit var storage: FirebaseStorage
+    lateinit var binding: AddpostLayoutBinding
 
-        //throw RuntimeException("################")
-
-        val rootRef = Firebase.storage.reference
-
-        val ref = rootRef.child("sangsangbugi.png")
-        ref.getBytes(Long.MAX_VALUE).addOnCompleteListener {
-            if (it.isSuccessful) {
-                val bmp = BitmapFactory.decodeByteArray(it.result,0,it.result!!.size)
-                val imgView = findViewById<ImageView>(R.id.imageView)
-                imgView.setImageBitmap(bmp)
-            }
-        }
+    companion object {
+        const val REQUEST_CODE = 1
+        const val UPLOAD_FOLDER = "sangsangbugi.png"
     }
+
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        binding = AddpostLayoutBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        Firebase.auth.currentUser ?: finish() //if not authenticated, finish this activity
+
+        storage = Firebase.storage
+        val storageRef = storage.reference // reference to root
+        val imageRef1 = storageRef.child("upload_images/background.png")
+        //imageRef1 indicate same object.
+
+    }
+
+
 }
